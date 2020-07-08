b1bb49eb565b87a7682f7905ae95c01c2cc3c7d9
Gustavo Barros <gustavoasb@gmail.com>
2020-07-07 23:31:19 -0300

    Create topbar level component

---
+++ src/components/TopBar/Level/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { LevelContainer } from "./styles";
+
+export default function Level({ value }) {
+
+  return (
+    <LevelContainer>
+      Nível {value}
+    </LevelContainer>
+  );
+}
---
+++ src/components/TopBar/Level/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const LevelContainer = styled.div`
+    font-size: 1.125rem;
+    color: #3399FF;
+    -webkit-text-stroke: 1px black;
+`
