e722b04d457568e79f6135330e67e93ea9f3e873
Gustavo Barros <gustavoasb@gmail.com>
2020-07-08 14:15:33 -0300

    Fix progress to occupie certain space

--- src/components/Progress/styles.js
+++ src/components/Progress/styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const ProgressContainer = styled.div`
     display: flex;
     align-items: center;
-    padding: 1rem;
+    height: 5%;
+    padding: 0 1rem;
     font-size: 1rem;
     color: #268604;
     -webkit-text-stroke: 1px black;
